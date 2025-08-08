d4c5307000711b81c785db792c22a39e987a6f5d
Daleyar <ali.daley72@gmail.com>
2025-08-07 23:11:17 -0400

    corrected GitHub pages workflow

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,19 @@ import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import "./index.css";
 
-const router = createBrowserRouter([
-  { path: "/", element: <App />, children: [
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <App />, children: [
       { index: true, element: <Home /> },
       { path: "projects", element: <Projects /> },
       { path: "experience", element: <Experience /> },
       { path: "contact", element: <Contact /> }
-  ]}
-]);
+    ]}
+  ],
+  {
+    basename: import.meta.env.BASE_URL
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><RouterProvider router={router} /></React.StrictMode>
